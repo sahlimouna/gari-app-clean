3fb0704da1f27912b34202ec6e8f18647997c774
sahlimouna <135168993+sahlimouna@users.noreply.github.com>
2025-07-04 11:28:37 +0100

    Update config.ts

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -6,14 +6,14 @@ import { getMessaging } from "firebase/messaging"
 import { getDatabase } from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyCv_yTxIDDIRTX4rWDwv_D2pPgYI33gWKk",
+  authDomain: "gari-parkinglocal.firebaseapp.com",
+  projectId: "ari-parkinglocal",
+  storageBucket:"gari-parkinglocal.firebasestorage.app",
+  messagingSenderId:"484158155675",
+  appId:"1:484158155675:web:9d69bcb0b10085b2cf0f3e",
+  measurementId:"G-Z58VSJBWLK",
+  databaseURL:"https://gari-parkinglocal-default-rtdb.firebaseio.com/",
 }
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig)
